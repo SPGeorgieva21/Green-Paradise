67b911aa888044d212f261c3973f5aba91da84e5
atpopov21 <atpopov21@codingburgas.bg>
2023-10-21 00:08:00 +0300

    Started game development
    Added basic interface mechanics

--- GreenParadise/GreenParadise/GreenParadise.cpp
+++ GreenParadise/GreenParadise/GreenParadise.cpp
@@ -1,19 +1,66 @@
 #include <iostream>
+#include <iomanip>
+#include <conio.h>
 #include "raylib.h"
 
-int main()
+int resolutionSettings()
 {
-	InitWindow(800, 600, "Algo-Rhythm_Testing");
-	SetWindowState(FLAG_VSYNC_HINT);
+	int resSize = 0;
+
+	std::cout <<"Before starting the game, please SELECT RESOLUTION SCALE: " << std::endl << "OPTIONS: 1 => 1280x720; 2 = > 1980x1080" << std::endl;
+
+	while ((resSize != 49) && (resSize != 50))
+	{
+		resSize = _getch();
+		if (resSize == 49)
+		{
+			system("cls");
+			return 1;
+		}
+		else if (resSize == 50)
+		{
+			system("cls");
+			return 2;
+		}
+	}
+}
+
+void gameRes1280x720()
+{
+	InitWindow(1280, 720, "Algo-Rhythm_Testing");
+	SetWindowState(FLAG_VSYNC_HINT);	//SetTargetFPS(60);
+
+	Texture2D background = LoadTexture("../../sprites/bg1280-720.png");
+	/*Texture2D earthWaterMap = LoadTexture("");
+	Texture2D earthGroundMap = LoadTexture("");*/
 
 	while (!WindowShouldClose())
 	{
 		BeginDrawing();
-		ClearBackground(DARKBLUE);
+		ClearBackground(BLACK);
+		
+		DrawTexture(background, 0, 0, WHITE);
+
 
 		DrawFPS(10, 10); //temporary
 		EndDrawing();
 	}
 
+	UnloadTexture(background);
 	CloseWindow();
+}
+
+void gameRes1980x1080()
+{
+
+}
+
+int main()
+{
+	switch (resolutionSettings())
+	{
+		case 1: gameRes1280x720(); break;
+		case 2: gameRes1980x1080(); break;
+		default: std::cout << "Error => INCORRECT INPUT." << std::endl << "Closing game...";
+	}
 }
