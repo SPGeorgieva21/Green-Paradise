68f7e502cbe9c7d7f0575e41c2494b5b011fae27
atpopov21 <atpopov21@codingburgas.bg>
2023-11-01 00:46:22 +0200

    Add falling stars

--- GreenParadise/GreenParadise/functions.cpp
+++ GreenParadise/GreenParadise/functions.cpp
@@ -39,38 +39,107 @@ void gameRes1280x720()
 
 	Texture2D background = LoadTexture("../../sprites/bg1280-720.png");
 	Texture2D earth = LoadTexture("../../sprites/worldRotation.png");
-	//Texture2D fallingStarEvent = LoadTexture("../../sprites/worldRotation.png");
+	Texture2D fallingStarEventL = LoadTexture("../../sprites/fallingStarL.png");
+	Texture2D fallingStarEventR = LoadTexture("../../sprites/fallingStarR.png");
 
 	float wordlFrameWidth = (float)(earth.width / 23);
-	int maxFrames = (int)(earth.width / (int)wordlFrameWidth);
+	int maxFramesE = (int)(earth.width / (int)wordlFrameWidth);
 
-	float timer = 0.0f;
-	int frame = 0;
+	float fallingStarFrameWidth = (float)(fallingStarEventL.width / 30);
+	int maxFramesFS = (int)(fallingStarEventL.width / (int)fallingStarFrameWidth);
+
+	float timerE = 0.0f;
+	int frameE = 0;
+
+	float timerFS = 0.0f;
+	int frameFS = 0;
+	int countDrawTime = 0;
+
+	float starFallingXdec = 0, starFallingYdec = 1, baseXFS = 550, baseYFS = -270;
+	bool checkIfOffScreen = false;
+	int fixFS = 0, countFixFS = 0;
+
+	// random direction of falling star
+	std::random_device rd2;
+	std::uniform_int_distribution <int> range2(0, 1);
 
 	while (!WindowShouldClose())
 	{
 		BeginDrawing();
 		ClearBackground(BLACK);
 
-		timer += GetFrameTime();
-		if (timer >= 0.2f)
+		timerE += GetFrameTime();
+		if (timerE >= 0.2f)
 		{
-			timer = 0.0f;
-			frame += 1;
+			timerE = 0.0f;
+			frameE += 1;
 		}
 
-		frame = frame % maxFrames;
-
-		/*std::random_device rd;
-		std::uniform_int_distribution <int> range(1, 3);
-
-		if (1 == range(rd))
-		{
-
-		}*/
+		frameE = frameE % maxFramesE;
 
 		DrawTexture(background, 0, 0, WHITE);
-		DrawTextureRec(earth, Rectangle{ (wordlFrameWidth * frame), 0, wordlFrameWidth, (float)earth.height }, Vector2{ (float)((GetScreenWidth() / 4) - 90), (float)50 }, RAYWHITE);
+
+		
+		if (!checkIfOffScreen)
+		{
+			countFixFS++;
+			if (countFixFS == 1)
+			{
+				fixFS = range2(rd2);
+			}
+			while (countDrawTime < 10)
+			{
+				countDrawTime++;
+
+				// timer for animation of falling star
+				timerFS += GetFrameTime();
+				if (timerFS >= 0.2f)
+				{
+					timerFS = 0.0f;
+					frameFS += 1;
+				}
+
+				frameFS = frameFS % maxFramesFS;
+
+				if (fixFS == true)
+				{
+					DrawTextureRec(fallingStarEventL, Rectangle{ (fallingStarFrameWidth * frameFS), 0, fallingStarFrameWidth, (float)fallingStarEventL.height }, Vector2{ (float)baseXFS - starFallingXdec, (float)baseYFS + starFallingYdec }, RAYWHITE);
+					starFallingXdec += 0.1;
+					starFallingYdec += 0.1;
+				}
+				else
+				{
+					DrawTextureRec(fallingStarEventR, Rectangle{ (fallingStarFrameWidth * frameFS), 0, fallingStarFrameWidth, (float)fallingStarEventL.height }, Vector2{ (float)baseXFS - starFallingXdec, (float)baseYFS + starFallingYdec }, RAYWHITE);
+					starFallingXdec -= 0.1;
+					starFallingYdec += 0.1;
+				}
+			}
+
+			countDrawTime = 0;
+		}
+		else
+		{
+			// random direction of falling star
+			std::random_device rd2;
+			std::uniform_int_distribution <int> range2(1, 2);
+			fixFS = range2(rd2);
+
+			// random X coordinates for the falling star
+			std::random_device rd;
+			std::uniform_int_distribution <int> range(250, 800);
+			baseXFS = range(rd);
+			baseYFS = -270;
+			starFallingXdec = 0;
+			starFallingYdec = 1;
+			checkIfOffScreen = false;
+		}
+		
+		if (starFallingYdec > (270 * 3))
+		{
+			checkIfOffScreen = true;
+		}
+
+		DrawTextureRec(earth, Rectangle{ (wordlFrameWidth * frameE), 0, wordlFrameWidth, (float)earth.height }, Vector2{ (float)((GetScreenWidth() / 4) - 90), (float)50 }, RAYWHITE);
 
 		DrawFPS(10, 10); //temporary
 		EndDrawing();
@@ -78,7 +147,8 @@ void gameRes1280x720()
 
 	UnloadTexture(background);
 	UnloadTexture(earth);
-	//UnloadTexture(fallingStarEvent);
+	UnloadTexture(fallingStarEventL);
+	UnloadTexture(fallingStarEventR);
 	CloseWindow();
 }
 
